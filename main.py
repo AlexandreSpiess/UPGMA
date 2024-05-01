3ca957ea7ed88fb22a8943b6575ed72ea5a95715
Alexandre Spiess <alexandre.spiess@epfl.ch>
2024-05-01 22:04:36 +0200

    Ajout de la class UMPGA

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import numpy as np
+
+import src.umpga as UMPGA
+
+
 def main():
     print("Début du programme principal")
+
+
+    arr = np.array([[1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]])
+    
+    UMPGA.hello()
+
+    umpga = UMPGA.UMPGA(arr)
+
+
     print("Fin du programme principal")
 
 
---
+++ src/umpga.py
@@ -0,0 +1,20 @@
+class UMPGA(object):
+
+    def __init__(self, arr):
+        self.arr=arr
+        print("int")
+
+
+
+    def min(self):
+        return np.argmin(self.arr)
+    
+    
+    
+    def compute(self):
+        ...
+
+
+
+def hello():
+    print("Hello from mon_module!")
